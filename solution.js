c89efa6acf0f1c0428471b2bb127c4bfdd8f13a5
Chanda Mulenga <stconeten@gmail.com>
2019-06-15 21:05:42 +0200

    Completes Task 1

--- solution.js
+++ solution.js
@@ -1,2 +1,30 @@
 // your solution should go here
 // for demo purposes use the console for any out you will need.
+
+// Task 1
+class Recipe {
+	constructor() {
+		this.title;
+		this.servings;
+		this.ingredients;
+	}
+
+	logInformation() {
+		console.log(this.title);
+		console.log(`Serves: ${this.servings}`);
+		let ingredientString = '';
+		this.ingredients.forEach(ingredient => {
+			ingredientString += `${ingredient} `;
+		});
+		console.log(`Ingredients: ${ingredientString}`);
+	}
+}
+
+const recipe = new Recipe();
+recipe.title = 'Mole';
+recipe.servings = 2;
+recipe.ingredients = ['cinnamon', 'cumin', 'cocoa'];
+
+recipe.logInformation();
+
+// Task 2
